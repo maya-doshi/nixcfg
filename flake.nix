c27dde4d7e9409c619e82c2bfff089be07693744
Maya Doshi <maya-doshi@proton.me>
2024-06-29 20:54:40 -0700

    Barebones basic

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "maya-nixcfg";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations = {
+        logan = lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./logan/configuration.nix
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.maya = import ./logan/home.nix;
+            }
+          ];
+        };
+      };
+    };
+}
