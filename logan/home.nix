c36c4c6c3f2322edda6ceb00d9a9ea610b469584
Maya Doshi <maya-doshi@proton.me>
2024-08-14 01:22:49 -0400

    foot home manager module

--- logan/home.nix
+++ logan/home.nix
@@ -70,6 +70,8 @@
       gnome.gnome-sound-recorder
       sqlitebrowser
       sqlite
+  imports = [
+    ../modules/home/wm/foot.nix
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -110,42 +112,4 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  programs.foot = {
-    enable = true;
-    server.enable = true;
-    settings = {
-      main = {
-        term = "xterm-256color";
-        shell = "fish";
-        font = "JetBrainsMono Nerd Font:size=12";
-        dpi-aware = "no";
-        workers = 12;
-      };
-      mouse = {
-        hide-when-typing = "yes";
-      };
-      # TODO: Change to declarative themeing
-      colors = {
-        alpha="0.9";
-        background="282828";
-        foreground="ebdbb2";
-        regular0="282828";
-        regular1="cc241d";
-        regular2="98971a";
-        regular3="d79921";
-        regular4="458588";
-        regular5="b16286";
-        regular6="689d6a";
-        regular7="a89984";
-        bright0="928374";
-        bright1="fb4934";
-        bright2="b8bb26";
-        bright3="fabd2f";
-        bright4="83a598";
-        bright5="d3869b";
-        bright6="8ec07c";
-        bright7="ebdbb2";
-      };
-    };
-  };
 }
