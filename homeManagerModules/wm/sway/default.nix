7bd7cfae40ef6e09dedf335f358cce481b57840e
Maya Doshi <maya-doshi@proton.me>
2024-08-28 09:17:20 -0400

    sway: bitwarden floating

--- homeManagerModules/wm/sway/default.nix
+++ homeManagerModules/wm/sway/default.nix
@@ -292,6 +292,7 @@
             { app_id = "dev.deedles.Trayscale"; }
             { app_id = "io.github.alainm23.planify"; }
             { app_id = ".blueman-manager-wrapped"; }
+            { app_id = "Bitwarden"; }
 
             { app_id = "firefox"; title = "Firefox - Sharing Indicator"; }
             { app_id = "firefox"; title = "Picture-in-Picture"; }
