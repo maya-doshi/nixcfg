d58aa2945318e455a1f9b819247d9a978f094e94
Maya Doshi <maya-doshi@proton.me>
2024-12-19 01:50:17 -0500

    sway: stop using foot server

--- homeManagerModules/wm/sway/default.nix
+++ homeManagerModules/wm/sway/default.nix
@@ -9,7 +9,7 @@
   config = lib.mkIf config.haze.windowManager.sway.enable {
     wayland.windowManager.sway = let
       mod = "Mod4";
-      term = "footclient";
+      term = "foot";
       termServer = "foot --server";
       web = "firefox";
       menu = "fuzzel";
@@ -39,7 +39,7 @@
         menu = "${menu}";
 
         startup = [
-          {command = "${termServer}";}
+          # {command = "${termServer}";}
           {command = "${notify}";}
         ];
 
