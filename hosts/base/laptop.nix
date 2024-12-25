caf7d20e4a5966976699fd6d0dbae7c6c80486cc
Maya Doshi <maya-doshi@proton.me>
2024-12-25 12:28:04 +0530

    logan: podman -> rootless docker

--- hosts/base/laptop.nix
+++ hosts/base/laptop.nix
@@ -69,12 +69,12 @@
   virtualisation.containers.enable = true;
   virtualisation = {
     libvirtd.enable = true;
-    podman = {
+    docker = {
       enable = true;
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
+      };
     };
   };
 
