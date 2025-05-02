7e016b56b13bbd8c0413359e504aeccd83ad8fec
Maya Doshi <maya-doshi@proton.me>
2025-05-02 12:06:47 -0400

    sioyek: colors and keybinds
    make more like zathura

--- homeManagerModules/apps/sioyek/default.nix
+++ homeManagerModules/apps/sioyek/default.nix
@@ -4,18 +4,23 @@
   config = lib.mkIf config.haze.apps.sioyek.enable {
     programs.sioyek = {
       enable = true;
-      # Just gruvbox
-      # https://mil.ad/blog/2022/gruvbox-for-sioyek.html
       config = {
         "custom_color_contrast" = "0.3";
-        "custom_color_mode_empty_background_color" = "#1d2021";
-        "page_separator_color" = "#1d2021";
-        "page_separator_width" = "10";
+        "custom_color_mode_empty_background_color" = "#282828";
+        "page_separator_color" = "#282828";
+        "page_separator_width" = "0";
         "custom_background_color" = "#282828";
-        "custom_text_color" = "#fbf1c7";
+        "custom_text_color" = "#ebdbb2";
         "search_highlight_color" = "#7ec16e";
-        "status_bar_color" = "#427b58";
-        "status_bar_text_color" = "#fbf1c7";
+        "status_bar_color" = "#8ec07c";
+        "status_bar_text_color" = "#282828";
+        "startup_commands" = "toggle_custom_color";
+      };
+      bindings = {
+        "next_page;goto_top_of_page" = "J";
+        "previous_page;goto_top_of_page" = "K";
+        "toggle_custom_color" = "<C-r>";
+        "toggle_two_page_mode" = "d";
       };
     };
   };
