b9d2c660465fba760bcb255b945086c55cbdca7d
Maya Doshi <maya-doshi@proton.me>
2025-09-08 22:07:43 -0400

    overlay: latest version of kavita and unlocked kavita plus
    from https://github.com/maya-doshi/Kavita/pull/1

--- overlays/unstable.nix
+++ overlays/unstable.nix
@@ -4,4 +4,14 @@ final: prev: {
   navidrome = prev.unstable.navidrome;
   memos = prev.unstable.memos;
   karakeep = prev.unstable.karakeep;
+  kavita = prev.unstable.kavita.overrideAttrs (old: {
+    backend = old.backend.overrideAttrs (oldBackend: {
+      patches = (oldBackend.patches or []) ++ [
+        (final.fetchpatch {
+          url = "https://patch-diff.githubusercontent.com/raw/maya-doshi/Kavita/pull/1.patch";
+          hash = "sha256-0Ug84gyiRPAUCnSpYRo5Tndq1j8z2b2LRc8rOwspFVw=";
+        })
+      ];
+    });
+  });
 }
