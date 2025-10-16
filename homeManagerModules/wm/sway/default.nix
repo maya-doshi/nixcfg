5c026cbf1fe8aad1e1976e8a779fedd10b09fe20
Maya Doshi <maya-doshi@proton.me>
2025-10-16 12:27:10 -0400

    sway: disable tocuhscreen

--- homeManagerModules/wm/sway/default.nix
+++ homeManagerModules/wm/sway/default.nix
@@ -168,12 +168,12 @@
           };
 
           "1267:11830:ELAN9008:00_04F3:2E36" = {
-            events = "enabled";
+            events = "disabled";
             map_to_output = "eDP-1";
           };
 
           "1267:11830:ELAN9008:00_04F3:2E36_Stylus" = {
-            events = "enabled";
+            events = "disabled";
             map_to_output = "eDP-1";
           };
         };
