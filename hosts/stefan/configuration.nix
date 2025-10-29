57655aec5e226e1dee3ea6015186a747592efa2e
Maya Doshi <maya-doshi@proton.me>
2025-10-29 14:13:48 -0400

    stefan: media group for cleaner perms

--- hosts/stefan/configuration.nix
+++ hosts/stefan/configuration.nix
@@ -9,7 +9,7 @@
     ../../overlays/kavita-service.nix
   ];
 
-  # Bootloader.
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
@@ -18,34 +18,42 @@
     hostId = "bc0e4252";
   };
 
-  # Set your time zone.
   time.timeZone = "America/New_York";
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users = {
-    haze = {
-      isNormalUser = true;
-      extraGroups = [ "networkmanager" "wheel" ];
-      packages = with pkgs; [];
+  users = {
+    groups = {
+      media = {
+        gid = 1000;
+      };
+      pine = {
+        name = "pine";
+        members = [ "pine" "haze" ];
+      };
     };
-    pine = {
-      isNormalUser = true;
-      createHome = true;
-      home = "/home/pine";
-      useDefaultShell = true;
-      group = "pine";
-      uid = 1001;
-      openssh.authorizedKeys.keys = [
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDhRFxpA7Ik6Gy8wwPq3SJrRCRLIXubWbbgzTO4ea7TsOBHhEgpU0dqqNcm3ON6WKMbnmrGD54jYURR4qXg3sqlC+SViSKmgyRt0EJBUcprZBIOvUshMQVtODaDgPMMGTSQCqc2ALDuUGdCClYYMSmwGDi9Q97YjpWChSeBcfxlaQpwY8KaU6IF/G1uu0NtkTJWmNOxDqAKiAPjLlLAMGi/AdIfNCmeACionYPVRI2mjaF9L+tUxW60I4cO3YiGAaum6lM4bJuVZtEqvBz0IQj2FFgUwyEsLwYrkfHR0f6q8X2hG/Zg6d6TTvmBIBnemofxypm7xc6R0jCJDoEE9OlUTpvltYiSRrizJdGsJJOLGBvhd6eAkPxjt5cVwEnr1R+svykyol6UorSB/w8T1w8u8dAwB28kxy4V2stZH1uo+kaF3bH57+8T3mkXJxC00BBfH077Mql1tgoR9gfh2r42U45AjHfUjuvfVISDUI46wlptrlgKnIug0kl5TJ0ESCE= pine@venus"
-      ];
-    };
-  };
-  haze.user = "haze";
 
-  users.groups.pine = {
-    name = "pine";
-    members = [ "pine" "haze" ];
+    users = {
+      haze = {
+        isNormalUser = true;
+        extraGroups = [ "networkmanager" "wheel" "media" ];
+        packages = with pkgs; [];
+      };
+      pine = {
+        isNormalUser = true;
+        createHome = true;
+        home = "/home/pine";
+        useDefaultShell = true;
+        uid = 1001;
+        group = "pine";
+        extraGroups = [ "media" ];
+
+        openssh.authorizedKeys.keys = [
+          "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDhRFxpA7Ik6Gy8wwPq3SJrRCRLIXubWbbgzTO4ea7TsOBHhEgpU0dqqNcm3ON6WKMbnmrGD54jYURR4qXg3sqlC+SViSKmgyRt0EJBUcprZBIOvUshMQVtODaDgPMMGTSQCqc2ALDuUGdCClYYMSmwGDi9Q97YjpWChSeBcfxlaQpwY8KaU6IF/G1uu0NtkTJWmNOxDqAKiAPjLlLAMGi/AdIfNCmeACionYPVRI2mjaF9L+tUxW60I4cO3YiGAaum6lM4bJuVZtEqvBz0IQj2FFgUwyEsLwYrkfHR0f6q8X2hG/Zg6d6TTvmBIBnemofxypm7xc6R0jCJDoEE9OlUTpvltYiSRrizJdGsJJOLGBvhd6eAkPxjt5cVwEnr1R+svykyol6UorSB/w8T1w8u8dAwB28kxy4V2stZH1uo+kaF3bH57+8T3mkXJxC00BBfH077Mql1tgoR9gfh2r42U45AjHfUjuvfVISDUI46wlptrlgKnIug0kl5TJ0ESCE= pine@venus"
+        ];
+      };
+    };
   };
+
+  haze.user = "haze";
 
   environment.systemPackages = with pkgs; [
     pass
@@ -57,6 +65,7 @@
   services = {
     plex = {
       enable = true;
+      group = "media";
       openFirewall = true;
     };
 
@@ -70,6 +79,7 @@
 
     kavita = {
       enable = true;
+      group = "media";
       tokenKeyFile = "/var/lib/kavita/tokenKey";
       settings.Port = 5000;
     };
@@ -82,6 +92,7 @@
         EnableInsightsCollector = true;
         MusicFolder = "/mnt/elena/media/music";
       };
+      group = "media";
       openFirewall = true;
       environmentFile = "/var/lib/navidrome/env";
     };
