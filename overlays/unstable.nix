ffcc582d4c10a72f173ef09fe4bf548f7a28aed4
Maya Doshi <maya-doshi@proton.me>
2025-12-07 14:50:05 -0500

    kavita: no longer unstable

--- overlays/unstable.nix
+++ overlays/unstable.nix
@@ -4,17 +4,17 @@ final: prev: {
   navidrome = prev.unstable.navidrome;
   memos = prev.unstable.memos;
   karakeep = prev.unstable.karakeep;
-  kavita = prev.unstable.kavita.overrideAttrs (old: {
+  kavita = prev.kavita.overrideAttrs (old: {
     backend = old.backend.overrideAttrs (oldBackend: {
       patches = (oldBackend.patches or []) ++ [
         (final.fetchpatch {
-          url = "https://patch-diff.githubusercontent.com/raw/maya-doshi/Kavita/pull/1.patch";
-          hash = "sha256-0Ug84gyiRPAUCnSpYRo5Tndq1j8z2b2LRc8rOwspFVw=";
+         url = "https://patch-diff.githubusercontent.com/raw/maya-doshi/Kavita/pull/1.patch";
+         hash = "sha256-0Ug84gyiRPAUCnSpYRo5Tndq1j8z2b2LRc8rOwspFVw=";
         })
-	(final.fetchpatch {
-	  url = "https://github.com/maya-doshi/Kavita/commit/4db108a58e85f11ed76ac9f3dcb435ce2d6fdd80.patch";
-	  hash = "sha256-KjEOU+FMb8sSKfOgnXKUvOwIxl/52V7lhGcwhgVw2DU=";
-	})
+        (final.fetchpatch {
+         url = "https://github.com/maya-doshi/Kavita/commit/4db108a58e85f11ed76ac9f3dcb435ce2d6fdd80.patch";
+         hash = "sha256-KjEOU+FMb8sSKfOgnXKUvOwIxl/52V7lhGcwhgVw2DU=";
+        })
       ];
     });
   });
