e14e3257ef5d063513710da9b09e3e4799419bfc
Maya Doshi <maya-doshi@proton.me>
2026-06-20 22:53:15 -0700

    stefan: add kernel modules

--- hosts/stefan/hardware-configuration.nix
+++ hosts/stefan/hardware-configuration.nix
@@ -7,7 +7,7 @@
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [ "kvm-amd" "zenpower" "msr" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
