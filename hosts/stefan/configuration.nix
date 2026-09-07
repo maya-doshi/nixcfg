cab2638efaf2ad05b7d011a1f71158a28f7260e2
Maya Doshi <maya-doshi@proton.me>
2026-09-07 11:59:31 -0700

    stefan: remove qui
    moving to docker compose with the qbit instances for consistent paths

--- hosts/stefan/configuration.nix
+++ hosts/stefan/configuration.nix
@@ -169,16 +169,6 @@
     };
 
     nginx.virtualHosts."cd.k4x.club".listen = [ { addr = "100.125.122.35"; port = 8765; } ]; # tailscale ip of stefan
-
-    qui = {
-      enable = true;
-      openFirewall = true;
-      secretFile = "/var/lib/qui/session-secret.txt";
-      settings = {
-        host = "100.125.122.35";
-        port = 7476;
-      };
-    };
   };
 
   haze.services = {
