f9e19cea5f5f7d367a97b3a62b0349452dfb318c
Maya Doshi <maya-doshi@proton.me>
2026-09-21 13:38:10 -0700

    sway: script fix

--- homeManagerModules/wm/i3blocks/scripts/battery.sh
+++ homeManagerModules/wm/i3blocks/scripts/battery.sh
@@ -1,38 +1,36 @@
 #!/usr/bin/env sh
 
-info=$(acpi -b | head -n 1)
-percent=$(echo "$info" | grep -oP '[0-9]+(?=%)')
-status=$(echo "$info" | awk '{print $3}' | tr -d ',')
+. /sys/class/power_supply/BAT0/uevent
 
-if [[ "$status" == "Charging" ]]; then
+if [ "$POWER_SUPPLY_STATUS" = "Charging" ]; then
     icon=""
-elif [[ "$status" == "Not" ]]; then
+elif [ "$POWER_SUPPLY_PRESENT" ]; then
     icon=""
-elif [[ "$percent" -ge 90 ]]; then
+elif [ "$POWER_SUPPLY_CAPACITY" -ge 90 ]; then
     icon=""
-elif [[ "$percent" -ge 70 ]]; then
+elif [ "$POWER_SUPPLY_CAPACITY" -ge 70 ]; then
     icon=""
-elif [[ "$percent" -ge 50 ]]; then
+elif [ "$POWER_SUPPLY_CAPACITY" -ge 50 ]; then
     icon=""
-elif [[ "$percent" -ge 20 ]]; then
+elif [ "$POWER_SUPPLY_CAPACITY" -ge 20 ]; then
     icon=""
 else
     icon=""
 fi
 
 color="#ebdbb2"
-if [ "$percent" -le 20 ]; then
+if [ "$POWER_SUPPLY_CAPACITY" -le 20 ]; then
     color="#fb4934"
 fi
 
-echo "${icon} ${percent}"
-echo "${percent}%"
+echo "${icon} ${POWER_SUPPLY_CAPACITY}"
+echo "${POWER_SUPPLY_CAPACITY}%"
 echo "$color"
 
-if [ "$percent" -le 10 ]; then
+if [ "$POWER_SUPPLY_CAPACITY" -le 10 ]; then
     exit 33
 fi
 
 case "$BLOCK_BUTTON" in
-    1) notify-send "Battery Info" "$info" ;;
+    1) notify-send "Battery Info" "$POWER_SUPPLY_NAME: $POWER_SUPPLY_CAPACITY%" ;;
 esac
